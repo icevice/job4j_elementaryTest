c1a7bae12d531e3a9b4a28f31268d5d7f6d4c603
Alexandr <icevice24@gmail.com>
2022-09-28 23:40:37 +0700

    7. Расстояние между точками в системе координат [#188 #124542]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int sixDivTwo = six / two;
+        int onePlusTwo = one + two;
+        int fiveMinusTwo = six - two;
+        int fiveTimeTwo = four * two;
+        System.out.println(onePlusTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fiveTimeTwo);
+    }
+}
