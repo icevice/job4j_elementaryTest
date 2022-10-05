fc280218c3b742f582da3c0ff701cdc880dd66ae
Alexandr <icevice24@gmail.com>
2022-10-05 23:33:49 +0700

    1. Модульные тесты. [#159435 #124565]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert2000RblThen28dot57142857142857Euro() {
+        float in = 2000;
+        float expected = 28.57142857142857f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert0RblThen0Euro() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert140RblThen3dot3333Dlr() {
+        float in = 140;
+        float expected = 2.3333333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert0RblThen0dlr() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
