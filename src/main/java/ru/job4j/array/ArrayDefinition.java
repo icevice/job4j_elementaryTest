663a0759a62902467c309adfb1d852980fe8cac4
Alexandr <icevice24@gmail.com>
2022-11-11 20:22:48 +0700

    6.0.3. Заполнение массива. [#226300 #191394]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Alexandr";
+        names[1] = "Lubov";
+        names[2] = "Eva";
+        names[3] = "Alexandra";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
