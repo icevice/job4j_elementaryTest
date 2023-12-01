aad96f0369fc4e0c180490517c00f2968a70f075
Alexandr <icevice24@gmail.com>
2023-12-01 23:47:25 +0700

    6.8. Метод двух указателей [#225 #310051]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -10,4 +10,20 @@ public class Check {
         }
         return result;
     }
+
+    public static int[] compress(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != 0) {
+                        array[i] = array[j];
+                        array[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
 }
--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -1,34 +1,44 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckTest {
     @Test
     void whenDataMonoByTrueThenTrue() {
-        boolean[] data = new boolean[] {true, true, true};
+        boolean[] data = new boolean[]{true, true, true};
         boolean result = Check.mono(data);
         assertThat(result).isTrue();
     }
 
     @Test
     void whenDataNotMonoByTrueThenFalse() {
-        boolean[] data = new boolean[] {true, false, true};
+        boolean[] data = new boolean[]{true, false, true};
         boolean result = Check.mono(data);
         assertThat(result).isFalse();
     }
 
     @Test
     void whenDataMonoByFalseThenTrue() {
-        boolean[] data = new boolean[] {false, false, false};
+        boolean[] data = new boolean[]{false, false, false};
         boolean result = Check.mono(data);
         assertThat(result).isTrue();
     }
 
     @Test
     void whenDataNotMonoByFalseThenFalse() {
-        boolean[] data = new boolean[] {false, true, false};
+        boolean[] data = new boolean[]{false, true, false};
         boolean result = Check.mono(data);
         assertThat(result).isFalse();
+    }
+
+    @Test
+    void compress() {
+        int[] array = new int[]{1, 2, 0, 3, 0, 4, 5};
+        int[] expected = new int[]{1, 2, 3, 4, 5, 0, 0};
+        int[] rsl = Check.compress(array);
+        assertThat(rsl).containsExactly(expected);
+
     }
 }
